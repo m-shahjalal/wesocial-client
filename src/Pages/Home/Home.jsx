b9ac7ad2841c69b7fe080b61cac7004213c8eb7c
MdSaifurRahaman <md.saifurrahaman.cse@gmail.com>
2022-01-02 02:58:08 +0600

    login pages added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import { Box, Container } from '@chakra-ui/react';
 import React from 'react';
+import NavBar from '../Shared/NavBar/NavBar';
 
 const Home = () => {
 	return (
 		<Container>
+			<NavBar />
 			<Box bg='tomato' w='100%' p={4} color='white'>
 				This is the Box
 			</Box>
--- src/app/Routers.jsx
+++ src/app/Routers.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
-import Registration from '../Pages/Registration/Registration';
+// import Registration from '../Pages/Registration/Registration';
 
 const Routers = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route index element={<Home />} />
-				<Route path='teams' element={<Login />} />
-				<Route path='teams' element={<Registration />} />
+				<Route path='login' element={<Login />} />
+				{/* <Route path='teams' element={<Registration />} /> */}
 			</Routes>
 		</BrowserRouter>
 	);
