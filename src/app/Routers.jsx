df674a7bb93ad022b40dee9128c322c4c1d50791
Reazour Rahman <alexrabbi111@gmail.com>
2022-01-03 12:55:57 +0600

    Forum-ui

--- src/app/Routers.jsx
+++ src/app/Routers.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Forum from '../Pages/Home/Forum/Forum';
 import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
 import Registration from '../Pages/Registration/Registration';
@@ -10,6 +11,7 @@ const Routers = () => {
 				<Route index element={<Home />} />
 				<Route path='teams' element={<Login />} />
 				<Route path='teams' element={<Registration />} />
+				<Route path='forum' element={<Forum />} />
 			</Routes>
 		</BrowserRouter>
 	);
